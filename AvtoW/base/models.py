9ed316f35947e7ccac842cc15a5379302f01030e
Rik Rims <Kirill.smirik1@ya.ru>
2022-09-26 03:31:55 +0500

    v0.1

--- AvtoW/base/models.py
+++ AvtoW/base/models.py
@@ -4,58 +4,85 @@ from django.db import models
 class Brand(models.Model):
     name = models.CharField("Марка", max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Model(models.Model):
     name = models.CharField("Модель", max_length=100)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class Contractor(models.Model):
     name = models.CharField("Поставщик", max_length=100)
     address = models.CharField("Адрес", max_length=250)
-    inn = models.CharField("ИНН", max_length=10)
-    phone = models.CharField("Номер телефона", max_length=15)
+    inn = models.CharField("ИНН", max_length=10, unique=True)
+    phone = models.CharField("Номер телефона", max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Manufacturer(models.Model):
     name = models.CharField("Производитель", max_length=100)
     address = models.CharField("Адрес", max_length=250)
-    inn = models.CharField("ИНН", max_length=10)
-    phone = models.CharField("Номер телефона", max_length=15)
+    inn = models.CharField("ИНН", max_length=10, unique=True)
+    phone = models.CharField("Номер телефона", max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
     name = models.CharField("Наименование", max_length=150)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
-    article = models.SlugField("Артикул")
+    article = models.SlugField("Артикул", unique=True)
     name = models.CharField("Наименование", max_length=150)
-    price = models.DecimalField("Цена", max_digits=10, decimal_places=10)
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
     description = models.TextField("Описание")
     amount = models.IntegerField("Количество")
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT)
     contractor = models.ForeignKey(Contractor, on_delete=models.PROTECT)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
     model = models.ForeignKey(Model, on_delete=models.PROTECT)
-    foto = models.ImageField(upload_to="photos/")
+    foto = models.ImageField(upload_to="photos/", blank=True, verbose_name="Фото")
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
 
 
 class Client(models.Model):
     first_name = models.CharField("Имя", max_length=25)
     last_name = models.CharField("Фамилия", max_length=25)
     address = models.CharField("Адрес", max_length=250)
-    phone = models.CharField("Номер телефона", max_length=15)
-    email = models.EmailField()
+    phone = models.CharField("Номер телефона", max_length=15, unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.PROTECT)
     date_order = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Order_Item(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     amount = models.IntegerField("Количество")
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
